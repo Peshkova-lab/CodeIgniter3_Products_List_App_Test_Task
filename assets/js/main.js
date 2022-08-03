b1722f4ca56d66cc817a7af911f64e8470b9141c
Peshkova-lab <kristino4ka928@gmail.com>
2022-08-04 02:46:26 +0300

    Added comments in main.js file.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
     change_filters_value();
 });
 
+// Функция срабатывает в случае взаимодействия с одним из фильтров и смене его значения.  
+// Считываются оба фильтра в независимости от того какой вызвал событие.
+// Значения записаны в переменные и передаються в контроллер с помощью ajax запроса.
+// В случае успеха результат записывается в блок списка.
 function change_filters_value() {
 
     var status = document.getElementById('status').value;
@@ -29,14 +33,14 @@ function change_filters_value() {
     })
 }
 
+// Функция срабатывает при взамодействии с чекбоксами элементов списка.
+// Считывается айди элемента, который вызвал событие, и логическая переменная - значение поля (сняли галочку или поставили).
+// Данные передаються в контроллер.
 function check_product(event) {
 
     var element = event.target;
     var id = element.id.substring(1);
     var status = element.checked; 
-
-    console.log(id);
-    console.log(status);
 
     $.ajax(
         {
@@ -48,20 +52,19 @@ function check_product(event) {
             'id': id,
             'status': status,
         },
-        success: function(result){
+        success: function(){
             console.log("Success!");
             change_filters_value();
-           
         },
         error: function(result) {
             console.log("Failed!");
             console.log(result);
         }
     })
-
-
 }
 
+// Удаление продукта из списка. Считывает айди на котором сработало событие. И с помощью аjаx запроса передает в контроллер.
+// При успешном удаленнии элемент удаляется вместе с родительским компонентом - блоком, без обновления страницы. 
 function delete_product(event) {
     var id = event.target.id;
     var delete_element = event.target.parentElement;
